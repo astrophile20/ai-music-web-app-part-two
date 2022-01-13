235bc8317f50dde8986c792f3c166a61d9c925e3
Ankita Ganguly <87467098+astrophile20@users.noreply.github.com>
2022-01-13 20:26:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+song1 = "";
+song2 = "";
+
+function preload() {
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
